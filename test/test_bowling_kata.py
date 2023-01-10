852974e685dc5fb1d6ca09dfa330411ed8e44580
Sebastián Estacio R <80277545+z0s3r77@users.noreply.github.com>
2023-01-10 03:22:58 +0100

    feature(test_bowling_kata.py): Añadido tests
    Se han añadido todos los casos test que debía superar el Kata.

--- test/test_bowling_kata.py
+++ test/test_bowling_kata.py
@@ -1,5 +1,5 @@
 import pytest
-from src.counterCard import ScoreCard
+from src.counterCardDict import ScoredCard
 
 
 
@@ -7,7 +7,7 @@ def test_0Scores():
 
     pins = "--------------------"
     total = 0
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
 
@@ -15,7 +15,7 @@ def test_TotalScoreHisttingPinsTest():
 
     pins = "12345123451234512345"
     total = 60
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
 
@@ -24,13 +24,13 @@ def test_TotalScoreHittingPinsFailTest():
 
     pins = "9-9-9-9-9-9-9-9-9-9-"
     total = 90
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
     
     pins = "9-3561368153258-7181"
     total = 82
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
 
@@ -39,20 +39,65 @@ def test_TotalScoreSpareTest():
 
     pins = "5/5/5/5/5/5/5/5/5/5/5"
     total = 150
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
 
     pins = "9-3/613/815/-/8-7/8/8"
     total = 131
-    scoreCard = ScoreCard(pins)
+    scoreCard = ScoredCard(pins)
     assert total == scoreCard.getTotalScore()
 
 
-# def test_TotalScoreStrikeTest():
 
-#     pins = "X9-9-9-9-9-9-9-9-9-"
-#     total = 100
-#     scoreCard = ScoreCard(pins)
-#     assert total == scoreCard.getTotalScore()
+def test_TotalScoreStrikeTest():
+
+    pins = "X9-9-9-9-9-9-9-9-9-"
+    total = 100
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+    pins = "9-9-9-9-9-9-9-9-9-X9-"
+    total = 100
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+    pins = "X9-X9-9-9-9-9-9-9-"
+    total = 110
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+    pins = "XX9-9-9-9-9-9-9-9-"
+    total = 120
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+    pins = "XXX9-9-9-9-9-9-9-"
+    total = 141
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+
+    pins = "9-9-9-9-9-9-9-9-9-XXX"
+    total = 111
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+
+    pins = "XXXXXXXXXXXX"
+    total = 300
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+
+    pins = "8/549-XX5/53639/9/X"
+    total = 149
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
+
+
+    pins = "X5/X5/XX5/--5/X5/"
+    total = 175
+    scoreCard = ScoredCard(pins)
+    assert total == scoreCard.getTotalScore()
 
